5e75721efff3d0925b4e0978d81de1adf72109b8
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2021-04-07 11:26:15 +0300

    better typing

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
 declare module 'react-native-widget-reload' {
-  const nativeModule: {
-    reloadAllWidgets: () => void;
-    androidConfigure: (widgetClassName: string, widgetIdsExtra: string) => void;
+  export default class WidgetReload {
+    reloadAllWidgets(): void;
+    androidConfigure(widgetClassName: string, widgetIdsExtra: string): void;
   }
-
-  export default nativeModule;
 }
